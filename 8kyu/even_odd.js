37f59924029afdb7cea2b9cb9aadc1f6dd0d005b
vladgrudak <ralkavladislav@gmail.com>
2024-09-28 11:18:30 +0300

    Odd or Even - Solution

---
+++ 8kyu/even_odd.js
@@ -0,0 +1,8 @@
+// TASK
+//Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+
+
+// SOLUTION
+function evenOrOdd(number) {
+    return (number % 2) ?  "Odd" :  "Even";
+}
